de3e4bae4d607306e85968bfb6f3afe819f49e14
Bryson Varner <brysonandmellisa@hotmail.com>
2023-09-04 14:32:04 -0500

    Completed PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var reversed ="";
+            for (int i = word.Length - 1;  i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+            var result = (reversed == word)? true : false;
+            return result;
+        }
+
+        // Make IsAPalindrome Method here
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData( "racecar", true)]
+        [InlineData( "animal", false)]
+        [InlineData( "rotator", true)]
+        [InlineData( "borrow", false)]
+        [InlineData( "level", true)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var result = test.IsAPalindrome(word);
+            //Assert
+            Assert.Equal(expected, result);
         }
     }
 }
